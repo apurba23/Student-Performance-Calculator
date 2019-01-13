78d8302b234f35153aba588d35d553158e26b1c1
Apurba Sarkar <apurbasarkar23@gmail.com>
2019-01-13 19:09:44 +0000

    Layout Part
    Main_Activity & Activity_login

--- app/src/main/java/com/example/undefined/studentsassistant/activity_login.java
+++ app/src/main/java/com/example/undefined/studentsassistant/activity_login.java
@@ -2,16 +2,12 @@ package com.example.undefined.studentsassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void LogOut(View view) {
+        setContentView(R.layout.activity_login);
     }
 }
